2a2ef5932b30d09fe3435e90a65507b115dbea77
tinvongoc-ct <tin.vongoc@chotot.vn>
2022-08-29 10:19:46 +0700

    fix: add prop interface to template

--- src/containers/Template/index.tsx
+++ src/containers/Template/index.tsx
@@ -1,14 +1,20 @@
+import React, { ReactNode } from "react";
 import Container from "@mui/material/Container";
 import Head from "next/head";
 import styles from "./styles.module.scss";
+interface Props {
+  children?: ReactNode;
+  title?: string;
+  // any props that come into the component
+}
 
-const Template = ({ children, title }) => {
+const Template = (props: Props) => {
   return (
     <Container maxWidth="lg" className={styles.container}>
       <Head>
-        <title>{title}</title>
+        <title>{props.title}</title>
       </Head>
-      {children}
+      {props.children}
     </Container>
   );
 };
